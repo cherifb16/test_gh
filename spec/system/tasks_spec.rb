c30919e959a0f4224bb7d9428796efbfac08aa15
cherifb16 <cherifbobbyray@gmail.com>
2022-10-13 12:20:09 +0200

    first commit

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'タスク管理機能', type: :system do
+  describe '登録機能' do
+    context 'タスクを登録した場合' do
+      it '登録したタスクが表示される' do
+        FactoryBot.create(:task)
+        visit tasks_path
+        expect(page).to have_content '書類作成'
+      end
+    end
+  end
+
+  describe '一覧表示機能' do
+    context '一覧画面に遷移した場合' do
+      it '登録済みのタスク一覧が表示される' do
+        FactoryBot.create(:task)
+        Task.all
+        visit tasks_path
+        expect(page).to have_content '書類作成'
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context '任意のタスク詳細画面に遷移した場合' do
+      it 'そのタスクの内容が表示される' do
+        task = FactoryBot.create(:task)
+        visit task_path(task.id)
+        expect(page).to have_content '書類作成'
+      end
+    end
+  end
+end
